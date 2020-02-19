f4ef8f4536596c69f860c7264c51e3d02116e273
romanenko <anshnine@gmail.com>
2020-02-20 03:31:21 +0500

    базовая структура класса Router.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
-// обшиее настройки
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+require "application/lib/dev.php";
 
-// подключение файлов системы
-define('ROOT', dirname(__FILE__));
-require_once(ROOT . '/components/Router.php');
+use application\core\Router;
+use application\lib\Db;
+
+// автозагрузка класса роутер
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\', '/', $class . ".php");
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
