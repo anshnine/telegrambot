dc1b8f102a28d936eeb66d913235c5ba30eac68e
romanenko <anshnine@gmail.com>
2020-02-20 02:22:52 +0500

    структура mvc

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// обшиее настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// подключение файлов системы
+define('ROOT', dirname(__FILE__));
+require_once(ROOT . '/components/Router.php');
